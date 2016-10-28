576058e1c0980fdc7e0cf364ad760104145f04aa
Jiao Hang <jiaohang.acc@gmail.com>
2016-10-28 09:30:54 +0800

    Extract callback

--- src/InStreamWrapper.h
+++ src/InStreamWrapper.h
@@ -3,77 +3,169 @@
 #include <Windows.h>
 #include <7zip/IStream.h>
 #include "stdio.h"
+#include <Common/MyCom.h>
+#include <7zip/IPassword.h>
+#include <7zip/Archive/IArchive.h>
+#include <string>
 
 
-	class InStreamWrapper : public IInStream, public IStreamGetSize
+class CArchiveExtractCallback :
+	public IArchiveExtractCallback,
+	public ICryptoGetTextPassword,
+	public CMyUnknownImp
+{
+public:
+	MY_UNKNOWN_IMP1(ICryptoGetTextPassword)
+
+		// IProgress
+		STDMETHOD(SetTotal)(UInt64 size) {
+		return S_OK;
+	}
+	STDMETHOD(SetCompleted)(const UInt64 *completeValue) {
+		return S_OK;
+	}
+
+	// IArchiveExtractCallback
+	STDMETHOD(GetStream)(UInt32 index, ISequentialOutStream **outStream, Int32 askExtractMode);
+	STDMETHOD(PrepareOperation)(Int32 askExtractMode);
+	STDMETHOD(SetOperationResult)(Int32 resultEOperationResult);
+
+	// ICryptoGetTextPassword
+	STDMETHOD(CryptoGetTextPassword)(BSTR *aPassword);
+
+private:
+	CMyComPtr<IInArchive> _archiveHandler;
+	//FString _directoryPath;  // Output directory
+	//UString _filePath;       // name inside arcvhive
+	//FString _diskFilePath;   // full path to file on disk
+	bool _extractMode;
+	struct CProcessedFileInfo
 	{
-	private:
+		FILETIME MTime;
+		UInt32 Attrib;
+		bool isDir;
+		bool AttribDefined;
+		bool MTimeDefined;
+	} _processedFileInfo;
 
-		long				m_refCount;
-		IStream*	m_baseStream;
+	//COutFileStream *_outFileStreamSpec;
+	CMyComPtr<ISequentialOutStream> _outFileStream;
 
-	public:
+public:
+	void Init(IInArchive *archiveHandler);
 
-		HRESULT STDMETHODCALLTYPE InStreamWrapper::QueryInterface(REFIID iid, void** ppvObject)
+	UInt64 NumErrors;
+	bool PasswordIsDefined;
+	//UString Password;
+
+	CArchiveExtractCallback() : PasswordIsDefined(false) {}
+};
+
+class ArchiveOpenCallback :
+	public IArchiveOpenCallback,
+	public ICryptoGetTextPassword,
+	public CMyUnknownImp
+{
+public:
+	MY_UNKNOWN_IMP1(ICryptoGetTextPassword)
+
+
+	STDMETHOD(SetTotal)(const UInt64 *files, const UInt64 *bytes) {
+		return S_OK;
+	}
+	STDMETHOD(SetCompleted)(const UInt64 *files, const UInt64 *bytes) {
+		return S_OK;
+	}
+
+	STDMETHOD(CryptoGetTextPassword)(BSTR *password) {
+		if (!PasswordIsDefined)
 		{
-			return E_NOINTERFACE;
+			// You can ask real password here from user
+			// Password = GetPassword(OutStream);
+			// PasswordIsDefined = true;
+			//PrintError("Password is not defined");
+			return E_ABORT;
 		}
+		return E_ABORT;
+		//return StringToBstr(Password, password);
+	}
 
-		ULONG STDMETHODCALLTYPE InStreamWrapper::AddRef()
-		{
-			return static_cast<ULONG>(InterlockedIncrement(&m_refCount));
-		}
 
-		ULONG STDMETHODCALLTYPE InStreamWrapper::Release()
-		{
-			ULONG res = static_cast<ULONG>(InterlockedDecrement(&m_refCount));
-			if (res == 0)
-			{
-				delete this;
-			}
-			return res;
-		}
+	bool PasswordIsDefined = false;
+	std::string Password;
+};
 
+
+class InStreamWrapper : public IInStream, public IStreamGetSize, public CMyUnknownImp
+{
+private:
+
+	long				m_refCount;
+	IStream*	m_baseStream;
+
+public:
+	MY_UNKNOWN_IMP1(IStreamGetSize)
+		/*
+				HRESULT STDMETHODCALLTYPE InStreamWrapper::QueryInterface(REFIID iid, void** ppvObject)
+				{
+					return E_NOINTERFACE;
+				}
+
+				ULONG STDMETHODCALLTYPE InStreamWrapper::AddRef()
+				{
+					return static_cast<ULONG>(InterlockedIncrement(&m_refCount));
+				}
+
+				ULONG STDMETHODCALLTYPE InStreamWrapper::Release()
+				{
+					ULONG res = static_cast<ULONG>(InterlockedDecrement(&m_refCount));
+					if (res == 0)
+					{
+						delete this;
+					}
+					return res;
+				}
+		*/
 		InStreamWrapper(IStream*	baseStream) {
-			m_baseStream = baseStream;
+		m_baseStream = baseStream;
+	}
+
+	// ISequentialInStream
+	STDMETHOD(Read)(void* data, UInt32 size, UInt32* processedSize) {
+		ULONG read = 0;
+		HRESULT hr = m_baseStream->Read(data, size, &read);
+		if (processedSize != NULL)
+		{
+			*processedSize = read;
 		}
+		// Transform S_FALSE to S_OK
+		return SUCCEEDED(hr) ? S_OK : hr;
+	}
 
-		// ISequentialInStream
-		STDMETHOD(Read)(void* data, UInt32 size, UInt32* processedSize) {
-			ULONG read = 0;
-			HRESULT hr = m_baseStream->Read(data, size, &read);
-			if (processedSize != NULL)
-			{
-				*processedSize = read;
-			}
-			// Transform S_FALSE to S_OK
-			return SUCCEEDED(hr) ? S_OK : hr;
+	// IInStream
+	STDMETHOD(Seek)(Int64 offset, UInt32 seekOrigin, UInt64* newPosition) {
+		LARGE_INTEGER move;
+		ULARGE_INTEGER newPos;
+
+		move.QuadPart = offset;
+		HRESULT hr = m_baseStream->Seek(move, seekOrigin, &newPos);
+		if (newPosition != NULL)
+		{
+			*newPosition = newPos.QuadPart;
 		}
+		return hr;
+	}
 
-		// IInStream
-		STDMETHOD(Seek)(Int64 offset, UInt32 seekOrigin, UInt64* newPosition) {
-			LARGE_INTEGER move;
-			ULARGE_INTEGER newPos;
 
-			move.QuadPart = offset;
-			HRESULT hr = m_baseStream->Seek(move, seekOrigin, &newPos);
-			if (newPosition != NULL)
-			{
-				*newPosition = newPos.QuadPart;
-			}
-			return hr;
+	// IStreamGetSize
+	STDMETHOD(GetSize)(UInt64* size) {
+		STATSTG statInfo;
+		HRESULT hr = m_baseStream->Stat(&statInfo, STATFLAG_NONAME);
+		if (SUCCEEDED(hr))
+		{
+			*size = statInfo.cbSize.QuadPart;
 		}
+		return hr;
+	}
 
-
-		// IStreamGetSize
-		STDMETHOD(GetSize)(UInt64* size) {
-			STATSTG statInfo;
-			HRESULT hr = m_baseStream->Stat(&statInfo, STATFLAG_NONAME);
-			if (SUCCEEDED(hr))
-			{
-				*size = statInfo.cbSize.QuadPart;
-			}
-			return hr;
-		}
-
-	};
+};
--- src/SevenZipHandler.cpp
+++ src/SevenZipHandler.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <Common/MyCom.h>
 using namespace NWindows;
 std::map<std::pair<uint32_t, uint64_t>, std::string> DumpToolLibInfos{
 	{ { 0x246ECE3C, 87866 }, "MaiDumpTool v233.2zEX v233.2z10" },
@@ -18,6 +19,7 @@ std::map<std::pair<uint32_t, uint64_t>, std::string> DumpToolLibInfos{
 	{ { 0x4FD25E74, 110535 }, "Vitamin Leaked Version" },
 };
 
+
 void SevenZipHandler::Open()
 {
 	const char* filePath = "PCSE00575_FULLGAME_01.00.VPK";
@@ -28,9 +30,9 @@ void SevenZipHandler::Open()
 		return;
 	}
 
-	auto inStream = new InStreamWrapper(fileStream);
+	CMyComPtr<InStreamWrapper> inStream = new InStreamWrapper(fileStream);
 
-	IInArchive* archive = (IInArchive*)CreateObject(CLSID_CFormatZip, IID_IInArchive);
+	CMyComPtr<IInArchive> archive = (IInArchive*)CreateObject(CLSID_CFormatZip, IID_IInArchive);
 
 	if (!archive)
 	{
@@ -38,8 +40,11 @@ void SevenZipHandler::Open()
 		return;
 	}
 
+	CMyComPtr<ArchiveOpenCallback> openCallbackSpec = new ArchiveOpenCallback;
+
+
 	const UInt64 scanSize = 1 << 23;
-	if (archive->Open(inStream, &scanSize, nullptr) != S_OK)
+	if (archive->Open(inStream, &scanSize, openCallbackSpec) != S_OK)
 	{
 		//PrintError("Can not open file as archive", archiveName);
 		return;
@@ -92,8 +97,6 @@ void SevenZipHandler::Open()
 
 		infoList.push_back(info);
 	}
-	archive->Close();
-	archive->Release();
 
 	std::sort(infoList.begin(), infoList.end(), [](const ArchiveItemInfo& a, const ArchiveItemInfo& b) {return a.path < b.path;});
 
@@ -135,6 +138,42 @@ void SevenZipHandler::Open()
 			FindDumpToolLib(infoList, vitaminLibPath, dumpTool);
 		}
 	}
+
+	CArchiveExtractCallback *extractCallbackSpec = new CArchiveExtractCallback;
+	CMyComPtr<IArchiveExtractCallback> extractCallback(extractCallbackSpec);
+	extractCallbackSpec->Init(archive); // second parameter is output folder path
+	extractCallbackSpec->PasswordIsDefined = false;
+	// extractCallbackSpec->PasswordIsDefined = true;
+	// extractCallbackSpec->Password = L"1";
+
+	/*
+	const wchar_t *names[] =
+	{
+	L"mt",
+	L"mtf"
+	};
+	const unsigned kNumProps = sizeof(names) / sizeof(names[0]);
+	NCOM::CPropVariant values[kNumProps] =
+	{
+	(UInt32)1,
+	false
+	};
+	CMyComPtr<ISetProperties> setProperties;
+	archive->QueryInterface(IID_ISetProperties, (void **)&setProperties);
+	if (setProperties)
+	setProperties->SetProperties(names, values, kNumProps);
+	*/
+
+	HRESULT result = archive->Extract(NULL, (UInt32)(Int32)(-1), false, extractCallback);
+
+	if (result != S_OK)
+	{
+		//PrintError("Extract Error");
+		return;
+	}
+	archive->Close();
+	archive->Release();
+
 }
 
 void SevenZipHandler::FindDumpToolLib(std::vector<ArchiveItemInfo> &infoList, std::string libPath, std::string& dumpTool)
@@ -148,3 +187,225 @@ void SevenZipHandler::FindDumpToolLib(std::vector<ArchiveItemInfo> &infoList, st
 		}
 	}
 }
+
+
+void CArchiveExtractCallback::Init(IInArchive *archiveHandler)
+{
+	NumErrors = 0;
+	_archiveHandler = archiveHandler;
+	//_directoryPath = directoryPath;
+	//NName::NormalizeDirPathPrefix(_directoryPath);
+}
+
+
+STDMETHODIMP CArchiveExtractCallback::GetStream(UInt32 index,
+	ISequentialOutStream **outStream, Int32 askExtractMode)
+{
+	*outStream = 0;
+	_outFileStream.Release();
+
+	//{
+	//	// Get Name
+	//	NCOM::CPropVariant prop;
+	//	RINOK(_archiveHandler->GetProperty(index, kpidPath, &prop));
+
+	//	UString fullPath;
+	//	if (prop.vt == VT_EMPTY)
+	//		fullPath = kEmptyFileAlias;
+	//	else
+	//	{
+	//		if (prop.vt != VT_BSTR)
+	//			return E_FAIL;
+	//		fullPath = prop.bstrVal;
+	//	}
+	//	_filePath = fullPath;
+	//}
+
+	//if (askExtractMode != NArchive::NExtract::NAskMode::kExtract)
+	//	return S_OK;
+
+	//{
+	//	// Get Attrib
+	//	NCOM::CPropVariant prop;
+	//	RINOK(_archiveHandler->GetProperty(index, kpidAttrib, &prop));
+	//	if (prop.vt == VT_EMPTY)
+	//	{
+	//		_processedFileInfo.Attrib = 0;
+	//		_processedFileInfo.AttribDefined = false;
+	//	}
+	//	else
+	//	{
+	//		if (prop.vt != VT_UI4)
+	//			return E_FAIL;
+	//		_processedFileInfo.Attrib = prop.ulVal;
+	//		_processedFileInfo.AttribDefined = true;
+	//	}
+	//}
+
+	//RINOK(IsArchiveItemFolder(_archiveHandler, index, _processedFileInfo.isDir));
+
+	//{
+	//	// Get Modified Time
+	//	NCOM::CPropVariant prop;
+	//	RINOK(_archiveHandler->GetProperty(index, kpidMTime, &prop));
+	//	_processedFileInfo.MTimeDefined = false;
+	//	switch (prop.vt)
+	//	{
+	//	case VT_EMPTY:
+	//		// _processedFileInfo.MTime = _utcMTimeDefault;
+	//		break;
+	//	case VT_FILETIME:
+	//		_processedFileInfo.MTime = prop.filetime;
+	//		_processedFileInfo.MTimeDefined = true;
+	//		break;
+	//	default:
+	//		return E_FAIL;
+	//	}
+
+	//}
+	//{
+	//	// Get Size
+	//	NCOM::CPropVariant prop;
+	//	RINOK(_archiveHandler->GetProperty(index, kpidSize, &prop));
+	//	UInt64 newFileSize;
+	//	/* bool newFileSizeDefined = */ ConvertPropVariantToUInt64(prop, newFileSize);
+	//}
+
+
+	//{
+	//	// Create folders for file
+	//	int slashPos = _filePath.ReverseFind_PathSepar();
+	//	if (slashPos >= 0)
+	//		CreateComplexDir(_directoryPath + us2fs(_filePath.Left(slashPos)));
+	//}
+
+	//FString fullProcessedPath = _directoryPath + us2fs(_filePath);
+	//_diskFilePath = fullProcessedPath;
+
+	//if (_processedFileInfo.isDir)
+	//{
+	//	CreateComplexDir(fullProcessedPath);
+	//}
+	//else
+	//{
+	//	NFind::CFileInfo fi;
+	//	if (fi.Find(fullProcessedPath))
+	//	{
+	//		if (!DeleteFileAlways(fullProcessedPath))
+	//		{
+	//			PrintError("Can not delete output file", fullProcessedPath);
+	//			return E_ABORT;
+	//		}
+	//	}
+
+	//	_outFileStreamSpec = new COutFileStream;
+	//	CMyComPtr<ISequentialOutStream> outStreamLoc(_outFileStreamSpec);
+	//	if (!_outFileStreamSpec->Open(fullProcessedPath, CREATE_ALWAYS))
+	//	{
+	//		PrintError("Can not open output file", fullProcessedPath);
+	//		return E_ABORT;
+	//	}
+	//	_outFileStream = outStreamLoc;
+	//	*outStream = outStreamLoc.Detach();
+	//}
+	return S_OK;
+}
+
+STDMETHODIMP CArchiveExtractCallback::PrepareOperation(Int32 askExtractMode)
+{
+	_extractMode = false;
+	switch (askExtractMode)
+	{
+	case NArchive::NExtract::NAskMode::kExtract:  _extractMode = true; break;
+	};
+	//switch (askExtractMode)
+	//{
+	//case NArchive::NExtract::NAskMode::kExtract:  PrintString(kExtractingString); break;
+	//case NArchive::NExtract::NAskMode::kTest:  PrintString(kTestingString); break;
+	//case NArchive::NExtract::NAskMode::kSkip:  PrintString(kSkippingString); break;
+	//};
+	//PrintString(_filePath);
+	return S_OK;
+}
+
+STDMETHODIMP CArchiveExtractCallback::SetOperationResult(Int32 operationResult)
+{
+	switch (operationResult)
+	{
+	case NArchive::NExtract::NOperationResult::kOK:
+		break;
+	default:
+	{
+		NumErrors++;
+		//PrintString("  :  ");
+		//const char *s = NULL;
+		//switch (operationResult)
+		//{
+		//case NArchive::NExtract::NOperationResult::kUnsupportedMethod:
+		//	s = kUnsupportedMethod;
+		//	break;
+		//case NArchive::NExtract::NOperationResult::kCRCError:
+		//	s = kCRCFailed;
+		//	break;
+		//case NArchive::NExtract::NOperationResult::kDataError:
+		//	s = kDataError;
+		//	break;
+		//case NArchive::NExtract::NOperationResult::kUnavailable:
+		//	s = kUnavailableData;
+		//	break;
+		//case NArchive::NExtract::NOperationResult::kUnexpectedEnd:
+		//	s = kUnexpectedEnd;
+		//	break;
+		//case NArchive::NExtract::NOperationResult::kDataAfterEnd:
+		//	s = kDataAfterEnd;
+		//	break;
+		//case NArchive::NExtract::NOperationResult::kIsNotArc:
+		//	s = kIsNotArc;
+		//	break;
+		//case NArchive::NExtract::NOperationResult::kHeadersError:
+		//	s = kHeadersError;
+		//	break;
+		//}
+		//if (s)
+		//{
+		//	PrintString("Error : ");
+		//	PrintString(s);
+		//}
+		//else
+		//{
+		//	char temp[16];
+		//	ConvertUInt32ToString(operationResult, temp);
+		//	PrintString("Error #");
+		//	PrintString(temp);
+		//}
+	}
+	}
+
+	//if (_outFileStream)
+	//{
+	//	if (_processedFileInfo.MTimeDefined)
+	//		_outFileStreamSpec->SetMTime(&_processedFileInfo.MTime);
+	//	RINOK(_outFileStreamSpec->Close());
+	//}
+	//_outFileStream.Release();
+	//if (_extractMode && _processedFileInfo.AttribDefined)
+	//	SetFileAttrib(_diskFilePath, _processedFileInfo.Attrib);
+	//PrintNewLine();
+	return S_OK;
+}
+
+
+STDMETHODIMP CArchiveExtractCallback::CryptoGetTextPassword(BSTR *password)
+{
+	if (!PasswordIsDefined)
+	{
+		// You can ask real password here from user
+		// Password = GetPassword(OutStream);
+		// PasswordIsDefined = true;
+		//PrintError("Password is not defined");
+		return E_ABORT;
+	}
+	return E_ABORT;
+	//return StringToBstr(Password, password);
+}
+
